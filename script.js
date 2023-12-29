19c4c071a90be7e3f26cbff05ac3011ac099019e
ArturRochel <arturrochel@gmail.com>
2023-12-29 16:26:39 -0300

    menu hamburguer update / footer media / emabalagens media

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const hamburguer = document.getElementById("hamburguer");
 function toggleMenu() {
   const responsivo = document.getElementById("header-responsivo");
   responsivo.classList.toggle("active");
+  hamburguer.classList.toggle("active");
+  document.onclick = function (e) {
+    if (!responsivo.contains(e.target) && !hamburguer.contains(e.target)) {
+      responsivo.classList.remove("active");
+      hamburguer.classList.remove("active");
+    }
+  };
 }
 
 hamburguer.addEventListener("click", toggleMenu);
