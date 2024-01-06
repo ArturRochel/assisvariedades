55aceed7190439f9e91d7b1ca1937471a43d616a
ArturRochel <arturrochel@gmail.com>
2024-01-06 11:01:52 -0300

    initial transitions / width & height itens / update responsive / update footer

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ function toggleMenu() {
 }
 
 hamburguer.addEventListener("click", toggleMenu);
+
+// Animações
+const target = document.querySelectorAll("[data-anime]");
+const animationClass = "animado";
+
+function animando() {
+  const scrollY = window.scrollY + (window.innerHeight * 3) / 4;
+  target.forEach(function (e) {
+    if (scrollY > e.offsetTop) {
+      e.classList.add(animationClass);
+    } else {
+      e.classList.remove(animationClass);
+    }
+  });
+}
+
+animando();
+
+if (target.length) {
+  window.addEventListener("scroll", animando);
+}
